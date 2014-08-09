a279c295b2b8559910e6672e4535161cc43d3148
Steve Waweru <steviewez@gmail.com>
2014-08-09 10:42:16 +0300

    Working changing access db to csv

--- main.py
+++ main.py
@@ -1,30 +1,21 @@
 __author__ = 'steve_w'
 
-import csvkit
-import os
-import pyodbc  # pyodbc is great for accessing microsoft access databases.
+import sys, subprocess, os
 
-tables = []
 DBfile = os.getcwd() + '/maragua.mdb'  # Name of file here either .accdb or .mdb
-cnxn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+DBfile)
-cursor = cnxn.cursor()
-# cursor.tables() is responsible for getting all the tables within the mdb so that we can query each table
-for dbtable in cursor.tables():
-    tables.append(dbtable.table_name)
+table_names = subprocess.Popen(["mdb-tables", "-1", DBfile],
+                               stdout=subprocess.PIPE).communicate()[0]
+tables = table_names.split('\n')
 
-cursor.close()
-
-cursor = cnxn.cursor()
+# Dump each table as a CSV file using "mdb-export",
+# converting " " in table names to "_" for the CSV filenames.
 for table in tables:
-    sql = 'SELECT * FROM ' + table
-    cursor.execute(sql)
-    row = cursor.fetchone()
-    if row:
-        print row  # Here shall be implemented to write to file. Manipulation of the output to look like a csv.
-        savefile = table + '.csv'
-        with open(savefile, 'wb') as f:
-            writer = csvkit.writer(f)
-            writer.writerows(row)
+    if table != '':
+        filename = table.replace(" ", "_") + ".csv"
+        file = open(filename, 'w')
+        print("Dumping " + table)
+        contents = subprocess.Popen(["mdb-export", DBfile, table],
+                                    stdout=subprocess.PIPE).communicate()[0]
+        file.write(contents)
+        file.close()
 
-cursor.close()
-cnxn.close()
