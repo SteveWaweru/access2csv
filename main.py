ad3b9f88f75ce86bf314ad4f250d47440de0b398
Steve Waweru <steviewez@gmail.com>
2014-08-09 16:47:54 +0300

    include naming folder to save files

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 __author__ = 'steve_w'
 
-import sys, subprocess, os
+import subprocess, os
 
-DBfile = os.getcwd() + '/maragua.mdb'  # Name of file here either .accdb or .mdb
-table_names = subprocess.Popen(["mdb-tables", "-1", DBfile],
-                               stdout=subprocess.PIPE).communicate()[0]
+name_of_file = "maragua.mdb"
+DBfile = os.getcwd() + '/' + name_of_file  # Name of file here either .accdb or .mdb
+table_names = subprocess.Popen(["mdb-tables", "-1", DBfile], stdout=subprocess.PIPE).communicate()[0]
 tables = table_names.split('\n')
-
+folder_name = name_of_file.split('.')
+# This will be for saving the files into a certain folder.
+if os.path.exists(folder_name[0]):
+    print("file already exists.")
+else:
+    os.makedirs(folder_name[0])
+    os.chdir(folder_name[0])
 # Dump each table as a CSV file using "mdb-export",
 # converting " " in table names to "_" for the CSV filenames.
 for table in tables:
     if table != '':
         filename = table.replace(" ", "_") + ".csv"
-        file = open(filename, 'w')
+        access_file = open(filename, 'w')
         print("Dumping " + table)
-        contents = subprocess.Popen(["mdb-export", DBfile, table],
-                                    stdout=subprocess.PIPE).communicate()[0]
-        file.write(contents)
-        file.close()
+        contents = subprocess.Popen(["mdb-export", DBfile, table], stdout=subprocess.PIPE).communicate()[0]
+        access_file.write(contents)
+        access_file.close()
 
